c91454bd7837e93f478b42f2d11c1597862b4341
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-03 02:39:11 +0000

    only test mran_down once per test session

--- tests/testthat/setup-functions.R
+++ tests/testthat/setup-functions.R
@@ -1,9 +1,9 @@
 
 
-skip_if_mran_down <- function () {
+skip_if_mran_down <- memoise::memoise( function () {
   # small 17K package
   tempdir <- tempfile(pattern = "testing", tmpdir = normalizePath(tempdir()))
   dir.create(tempdir)
   prepare <- try(versions::install.versions("enrichwith", "0.0.3", lib = tempdir), silent = TRUE)
   if (class(prepare) == "try-error") skip("Couldn't download test package, MRAN may be down")
-}
+})
