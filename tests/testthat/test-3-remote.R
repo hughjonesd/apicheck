57ca145a9bf5a7355db19323468312d5643246ec
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-03 23:49:17 +0000

    testing tweaks

--- tests/testthat/test-3-remote.R
+++ tests/testthat/test-3-remote.R
@@ -1,21 +1,28 @@
 
 context("Remote tests")
 
+library(rlang)
+
 run_nicely <- function(cran, expr) {
-  old_opts <- options(pastapi.use_cran = cran)
+  expr <- enquo(expr)
+  old_opts <- options(pastapi.use_cran = cran, repos = "https://cloud.r-project.org")
   old_lib_dir <- set_lib_dir(NULL) # can't avoid possibly putting LIB_DIR into options...
   clear_package_cache()
   on.exit({
     options(old_opts)
     set_lib_dir(old_lib_dir)
   })
-  eval(substitute(expr))
+  eval_tidy(expr)
 }
 
+
 install_on <- function (cran, package, version) {
+  if (cran == FALSE) skip("MRAN not working for the moment...")
+  force(package)
+  force(version)
   run_nicely(cran,
-    expect_error(call_with_namespace(!!package, !!version, function (x) NULL, quiet = TRUE), NA,
-          info = paste("use_cran was", cran))
+    expect_error(call_with_namespace(package, version, function (x) NULL, quiet = TRUE), NA,
+          info = sprintf("cran: %s, package: %s, version: %s", cran, package, version))
   )
 }
 
@@ -36,6 +43,7 @@ test_that("Can install early and current versions using devtools, and early vers
   install_early_late("clipr") # no compilation
   install_early_late("alineR") # no dependencies & small but needs compilation
 })
+
 
 test_that("Can install versions when package already installed and loaded", {
   skip_on_cran()
@@ -58,5 +66,5 @@ test_that("Can install versions when package already installed and loaded", {
 test_that("Multiple parallel remote installs", {
   skip_on_cran()
 
-  run_nicely(when_fn_exists("clipr::dr_clipr", search = "parallel"))
+  run_nicely(cran = TRUE, when_fn_exists("clipr::dr_clipr", search = "parallel"))
 })
